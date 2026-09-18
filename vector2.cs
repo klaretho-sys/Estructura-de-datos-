887972c13d71ba15be0f3582606c102d17f2ccae
klaretho-sys <klaretho@gmail.com>
2026-09-18 14:31:41 -0500

    Create vector2.cs

---
+++ vector2.cs
@@ -0,0 +1,8 @@
+ string[] estudiantes =
+    { " Juan", "Klareth", "Sofia", "Gael", "Fran", "Rosalba"};
+
+
+            for (int i = 0; i < estudiantes.Length; i++)
+            {
+                Console.WriteLine($"Indice [{i}]:   {estudiantes[i]}");
+            }
