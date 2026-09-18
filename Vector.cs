af9645b71affc5312c62ba17f1b7cdbc3192eeb2
klaretho-sys <klaretho@gmail.com>
2026-09-18 13:07:58 -0500

    Create Vector.cs

---
+++ Vector.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace activity 1 kls
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[5]
+            { 10, 30, 5, -2, 66 };
+            Console.WriteLine( $"Elementos de [0]: {numeros[0]}");
+        }
+    }
+}
